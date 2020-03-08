b7fb8b305ca11d135551b2265f8714952102d693
Andreas Andersson <andreas@herrandersson.se>
2020-03-08 09:52:30 +0100

    Render an empty window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use amethyst::{
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
+
+extern crate amethyst;
+
+pub struct Pong;
+
+impl SimpleState for Pong {}
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let assets_dir = app_root.join("assets");
+    let config_dir = app_root.join("config");
+    let display_config_path = config_dir.join("display.ron");
+
+    let game_data = GameDataBuilder::default().with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+            // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)?
+                    .with_clear([0.0, 0.0, 0.0, 1.0]),
+            )
+            // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
+            .with_plugin(RenderFlat2D::default()),
+    )?;
+
+    let mut game = Application::new(assets_dir, Pong, game_data)?;
+    game.run();
+
+    Ok(())
 }
