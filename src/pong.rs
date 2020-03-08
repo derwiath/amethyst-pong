da69d786e2ab75757be211194df790573a6027dc
Andreas Andersson <andreas@herrandersson.se>
2020-03-08 10:15:41 +0100

    Set up camera

--- src/pong.rs
+++ src/pong.rs
@@ -1,7 +1,28 @@
 extern crate amethyst;
 
-use amethyst::prelude::*;
+use amethyst::{core::transform::Transform, prelude::*, renderer::Camera};
 
 pub struct Pong;
 
-impl SimpleState for Pong {}
+pub const ARENA_HEIGHT: f32 = 100.0;
+pub const ARENA_WIDTH: f32 = 100.0;
+
+fn initialise_camera(world: &mut World) {
+    // Setup camera in a way that our screen covers whole arena and (0, 0) is in the bottom left.
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(ARENA_WIDTH * 0.5, ARENA_HEIGHT * 0.5, 1.0);
+
+    world
+        .create_entity()
+        .with(Camera::standard_2d(ARENA_WIDTH, ARENA_HEIGHT))
+        .with(transform)
+        .build();
+}
+
+impl SimpleState for Pong {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+
+        initialise_camera(world);
+    }
+}
