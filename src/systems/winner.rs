d6233645fdd819af353041d576285c2e8d4ec93a
Andreas Andersson <andreas@herrandersson.se>
2020-03-31 13:45:52 +0200

    Update score board on each score

--- src/systems/winner.rs
+++ src/systems/winner.rs
@@ -1,34 +1,51 @@
 use amethyst::{
     core::transform::Transform,
     derive::SystemDesc,
-    ecs::prelude::{Join, System, SystemData, WriteStorage},
+    ecs::prelude::{Join, ReadExpect, System, SystemData, Write, WriteStorage},
+    ui::UiText,
 };
 
-use crate::pong::{Ball, ARENA_WIDTH};
+use crate::pong::{Ball, ScoreBoard, ScoreText, ARENA_WIDTH};
 
 #[derive(SystemDesc)]
 pub struct WinnerSystem;
 
 impl<'s> System<'s> for WinnerSystem {
-    type SystemData = (WriteStorage<'s, Ball>, WriteStorage<'s, Transform>);
+    type SystemData = (
+        WriteStorage<'s, Ball>,
+        WriteStorage<'s, Transform>,
+        WriteStorage<'s, UiText>,
+        Write<'s, ScoreBoard>,
+        ReadExpect<'s, ScoreText>,
+    );
 
-    fn run(&mut self, (mut balls, mut locals): Self::SystemData) {
+    fn run(
+        &mut self,
+        (mut balls, mut locals, mut ui_text, mut score_board, score_text): Self::SystemData,
+    ) {
         for (ball, transform) in (&mut balls, &mut locals).join() {
             let ball_x = transform.translation().x;
 
             let did_hit = if ball_x <= ball.radius {
                 // Right player scored on the left side.
-                println!("Player 2 Scores!");
+                score_board.right = (score_board.right + 1).min(999);
+                if let Some(text) = ui_text.get_mut(score_text.right) {
+                    text.text = score_board.right.to_string();
+                }
                 true
             } else if ball_x >= ARENA_WIDTH - ball.radius {
                 // Left player scored on the right side.
-                println!("Player 1 Scores!");
+                score_board.left = (score_board.left + 1).min(999);
+                if let Some(text) = ui_text.get_mut(score_text.left) {
+                    text.text = score_board.left.to_string();
+                }
                 true
             } else {
                 false
             };
 
             if did_hit {
+                println!("Scores: {:^3} - {:^3}", score_board.left, score_board.right);
                 ball.velocity[0] = -ball.velocity[0]; // Reverse Direction
                 transform.set_translation_x(ARENA_WIDTH / 2.0); // Reset Position
             }
